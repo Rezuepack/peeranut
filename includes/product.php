9499c630de8fe5b025dd2ab3fa29b56f66b17d6a
Rezuepack <premracing1@gmail.com>
2020-10-24 19:42:54 +0700

    fix bugs

--- includes/product.php
+++ includes/product.php
@@ -12,6 +12,7 @@
                             <div class="box-cart text-center">
                                 <img src="images/store/<?= $row['store_image']; ?>" width="100%" height="200">
                                 <p><?= $row['store_name']; ?></p>
+                                <p><?= number_format($row['store_price'], 2); ?></p>
                                 <?= '<a href="cart.php?store_id='.$row['store_id'].'&action=add" class="btn btn-custom"><i class="fas fa-cart-plus"></i> หยิบใส่ตระกร้าสินค้า</a>' ?>
                             </div>
                         </div>
